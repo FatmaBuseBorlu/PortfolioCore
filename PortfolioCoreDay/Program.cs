2ac8d9f802df6faca6c4fb4a620fbd7389c5f2cd
Fatma Buse Borlu <fatmabuseborlu@gmail.com>
2026-06-18 23:00:49 +0300

    Read SQL connection string from configuration

--- PortfolioCoreDay/Program.cs
+++ PortfolioCoreDay/Program.cs
@@ -3,20 +3,18 @@ using PortfolioCoreDay.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<PortfolioContext>();
-
+builder.Services.AddDbContext<PortfolioContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/Error/Page404");
 
 app.UseHttpsRedirection();
@@ -31,6 +29,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
-
-
